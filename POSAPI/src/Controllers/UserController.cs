e3f5367b1cda79ae84f0609baf577acd0afe495a
Nitesh Naik <x00145570@gmail.com>
2023-06-01 23:26:21 +0100

    created login method

---
+++ POSAPI/src/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using POSAPI.src;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace POSAPI.Controllers
+{
+    [Route("api/services")]                                 
+    [ApiController]
+    [Produces("application/json")]
+    public class UserController : ControllerBase
+    {
+        private readonly Model _model;
+
+        public UserController(Model model)
+        {
+            _model = model;
+        }
+
+        [HttpGet("Login")]
+        public ActionResult<String> Login([FromBody] UserRequest LoginDetails)
+        {
+            
+            var usern = _model.SystemUsers.Where(user => user.Username.Equals(LoginDetails.Username) && (user.Password.Equals(LoginDetails.Password)));
+
+            if (usern.Any() == true)
+            {
+                return Ok(new String("Successfully Logged in"));
+            }
+            return NotFound("Invalid Login details");    
+          
+        } 
+    }
+}
